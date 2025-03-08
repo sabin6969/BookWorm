c508297e5925fb0d9cff4666635ff80ddaf2f83c
sabin6969 <sabinpoudel6969@gmail.com>
2025-03-08 11:06:52 +0545

    Add internet permission and enhance BookSummaryView layout with author display and summary section

--- lib/view/summary/book_summary_view.dart
+++ lib/view/summary/book_summary_view.dart
@@ -23,6 +23,62 @@ class BookSummaryViewState extends State<BookSummaryView> {
           overflow: TextOverflow.ellipsis,
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.network(
+                widget.item.volumeInfo?.imageLinks?.thumbnail ?? "",
+              ),
+            ),
+            Chip(
+              label: Text(
+                "Author(s)",
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                spacing: 10,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: List.generate(
+                  (widget.item.volumeInfo?.authors.length) ?? 0,
+                  (index) => Chip(
+                    label: Text(
+                      widget.item.volumeInfo?.authors[index] ?? "N/A",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Summary",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Scrollbar(
+                thumbVisibility: true,
+                interactive: true,
+                thickness: 5,
+                child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Text(
+                    "This is the dummy book content " * 200,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
